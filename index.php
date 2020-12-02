828dfcfbff549b0b897ac101fe77c0f90eb20f4d
“Fideline <fideline.bazana_ntomo@edu.devinci.fr>
2020-12-02 19:08:55 +0100

    push

--- index.php
+++ index.php
@@ -5,73 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- ===== CSS ===== -->
-    <!-- 
-    <link rel="stylesheet" href="css/index.css"> -->
+    <link rel="stylesheet" href="./css/style_index.css">
     <title>Online Survey</title>
-    <style>
-        @media screen and (min-width: 768px) {
-
-            body {
-                margin: 0;
-                padding: 0;
-            }
-
-            h1 {
-                font-size: 50px;
-            }
-
-            img {
-                width: 100%;
-                height: auto;
-            }
-
-            a {
-                text-decoration: none;
-            }
-
-            .container {
-                display: flex;
-                justify-content: space-around;
-            }
-
-            .left {
-                margin-top: 35vh;
-                margin-left: 5vw;
-            }
-
-            .right {
-                width: 45vw;
-                margin-top: 50px;
-                margin-right: 5vw;
-            }
-
-            .left .loginButton {
-                margin: 0 12vw;
-            }
-
-            .loginButton {
-                display: block;
-                padding: 1rem;
-                margin: 2rem 0;
-                background-color: rgba(170, 12, 39, 0.4);
-                color: #FFF;
-                font-weight: 600;
-                text-align: center;
-                border-radius: .5rem;
-                transition: .3s;
-            }
-
-            .loginButton:hover {
-                background-color: #aa0c27;
-            }
-
-
-            .loginButton :hover {
-                background-color: #aa0c27;
-            }
-        }
-
-    </style>
 </head>
 
 <body>
