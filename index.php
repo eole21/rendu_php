4472b014cc9ef9d0dc09e237bf5523da020d6a9b
“Fideline <fideline.bazana_ntomo@edu.devinci.fr>
2020-12-02 17:56:00 +0100

    index

--- index.php
+++ index.php
@@ -1,31 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>| Online Quiz System |</title>
-        <link rel="stylesheet" type="text/css" href="css/index.css" />
-        <link rel="shortcut icon" type="image/png" href="image/logo.png" />
-        <style type="text/css">
-            body {
-                width: 100%;
-                background: url(image/book.png) ;
-                background-position: center center;
-                background-repeat: no-repeat;
-                background-attachment: fixed;
-                background-size: cover;
-            }
-        </style>
-    </head>
-    <body>
-        <center>
-            <div class="intro">
-                <h1> online quiz system </h1>
-                <a href="login.php" class="btn"> login </a> &emsp;
-                <a href="register.php" class="btn"> register </a>
-                <h2> Good &nbsp;Luck. </h2>
-            </div>
-        </center>
-    </body>
-</html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- ===== CSS ===== -->
+    <link rel="stylesheet" href="css/index.css">
+    <title>Online Survey</title>
+</head>
+
+<body>
+   <!-- container -->
+    <div class="container">
+        <div class="left">
+           <!-- Titre -->
+            <h1>THE ONLINE SURVEY</h1>
+            <!-- button lien -->
+            <a href="login.php" class="loginButton">Connexion</a>
+        </div>
+        <div class="right">
+           <!-- img -->
+            <img src="image/survey_checkin.svg" alt="survey image freepiks">
+        </div>
+    </div>
+</body></html>
